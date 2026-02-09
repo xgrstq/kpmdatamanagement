15e7792725dbbf997fb2f8b91a1a5c4443876097
Xgrstq <lowoxtreeme@gmail.com>
2026-02-09 20:54:40 +0700

    "fix: normalize pages filenames for linux build"

--- src/layouts/Mainlayout.jsx
+++ src/layouts/Mainlayout.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Sidebar from "../components/Sidebar";
-import Topbar from "../components/topbar";
+import Topbar from "../components/Topbar";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
